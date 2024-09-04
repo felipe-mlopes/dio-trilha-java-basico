f72c905494b1db03658ad02fd18f739560feba24
Felipe Lopes <60843113+felipe-mlopes@users.noreply.github.com>
2024-09-04 20:50:35 +0000

    feat: create ordering with map pratice

--- collections-list/src/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
+++ collections-list/src/map/Ordenacao/LivrariaOnline/LivrariaOnline.java
@@ -1,6 +1,8 @@
 package map.Ordenacao.LivrariaOnline;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -17,8 +19,16 @@ public class LivrariaOnline {
     }
 
     public void removerLivro(String titulo) {
-        if (!livrosMap.isEmpty()) {
-            livrosMap.remove(titulo);
+        List<String> chavesRemover = new ArrayList<>();
+
+        for (Map.Entry<String, Livro> entry : livrosMap.entrySet()) {
+            if (entry.getValue().getTitulo().equalsIgnoreCase(titulo)) {
+                chavesRemover.add(entry.getKey());
+            }
+        }
+
+        for (String chave : chavesRemover) {
+            livrosMap.remove(chave);
         }
     }
 
@@ -27,17 +37,66 @@ public class LivrariaOnline {
         System.out.println(livrosTreeMap);
     }
     
-    public void pesquisarLivrosPorAutor(String autor) {}
+    public void pesquisarLivrosPorAutor(String autor) {
+        if (!livrosMap.isEmpty()) {
+            for (Map.Entry<String, Livro> livro : livrosMap.entrySet()) {
+                if (livro.getValue().getAutor().equalsIgnoreCase(autor)) {
+                     System.out.println(livro.getValue());
+                }
+            }
+        } else {
+            System.out.println("O livro não foi encontrado.");
+        }
+    }
 
-    public void obterLivroMaisCaro() {}
+    public Livro obterLivroMaisCaro() {
+        Livro livroMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
 
-    public void exibirLivroMaisBarato() {}
+        if (!livrosMap.isEmpty()) {
+            for (Map.Entry<String, Livro> livro : livrosMap.entrySet()) {
+                if (livro.getValue().getPreco() > maiorPreco) {
+                    livroMaisCaro = livro.getValue();
+                    maiorPreco = livro.getValue().getPreco();
+                }
+            }
+        }
+
+        return livroMaisCaro;
+    }
+
+    public Livro exibirLivroMaisBarato() {
+        Livro livroMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+
+        if (!livrosMap.isEmpty()) {
+            for (Map.Entry<String, Livro> livro : livrosMap.entrySet()) {
+                if (livro.getValue().getPreco() < menorPreco) {
+                    livroMaisBarato = livro.getValue();
+                    menorPreco = livro.getValue().getPreco();
+                }
+            }
+        }
+
+        return livroMaisBarato;
+    }
 
     public static void main(String[] args) {
         LivrariaOnline livrariaOnline = new LivrariaOnline();
 
-        livrariaOnline.adicionarLivro("https://encurtador.com.br/JasMO", "Storytelling com dados: vamos praticar", "Cole Nussbaumer Knaflic", 67.80);
+        livrariaOnline.adicionarLivro("https://www.livraria.com/livro-01", "Livro 01", "Autor 01", 67.80);
+        livrariaOnline.adicionarLivro("https://www.livraria.com/livro-02", "Livro 02", "Autor 01", 45.90);
+        livrariaOnline.adicionarLivro("https://www.livraria.com/livro-03", "Livro 03", "Autor 02", 134.50);
+        livrariaOnline.adicionarLivro("https://www.livraria.com/livro-04", "Livro 04", "Autor 02", 20.35);
 
         livrariaOnline.exibirLivrosOrdenadosPorPreco();
+
+        livrariaOnline.pesquisarLivrosPorAutor("Autor 01");
+
+        livrariaOnline.removerLivro("Livro 04");
+        livrariaOnline.exibirLivrosOrdenadosPorPreco();
+
+        System.out.println("O livro mais caro é: " + livrariaOnline.obterLivroMaisCaro());
+        System.out.println("O livro mais caro é: " + livrariaOnline.exibirLivroMaisBarato());
     }
 }
